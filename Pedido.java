96f0a9e86c1cad27664a377633d9a7405d84ede5
HugoRQLL <huroquillan0525@gmail.com>
2025-10-19 18:12:38 -0500

    cambio

---
+++ Pedido.java
@@ -0,0 +1,9 @@
+public class Pedido {
+ private String nombrePlato;
+ public Pedido(String nombrePlato) {
+ this.nombrePlato = nombrePlato;
+ }
+ public String getNombrePlato() {
+ return nombrePlato;
+ }
+}
